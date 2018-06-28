7fdd753104b175617809218754cc8930b6996056
unknown <kiokoleon@gmail.com>
2018-06-28 14:55:02 +0300

    Update Config

--- config.py
+++ config.py
@@ -8,23 +8,22 @@ class Config(object):
     """Base class with all the constant config variables"""
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = getenv('SECRET_KEY')
-
 
 class TestingConfig(Config):
     """Contains additional config variables required during testing"""
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = getenv('TESTING_DATABASE_URI')
-   
-
-class DevelopmentConfig(Config):
-    """Contains additional config variables required during development"""
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = getenv('DEVELOPMENT_DATABASE_URI')
-
+    DATABASE = getenv('TESTING_DATABASE_NAME')
+    PASSWORD = getenv('PASSWORD')
+    HOST = getenv('HOST')
+    USER = getenv('USER')
 
 class ProductionConfig(Config):
     """Contains additional config variables required during production"""
-    DEBUG = False
+    DEBUG = True
+    TESTING = True
+    DATABASE = getenv('PRODUCTION_DATABASE_NAME')
+    PASSWORD = getenv('PASSWORD')
+    HOST = getenv('HOST')
+    USER = getenv('USER')
