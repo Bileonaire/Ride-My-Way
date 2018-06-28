cb37c85b44dbd45cd153c7a963f9b15d43a9059c
unknown <kiokoleon@gmail.com>
2018-06-28 16:53:08 +0300

    Fix Travis build

--- .travis.sh
+++ .travis.sh
@@ -1,2 +1,2 @@
 export SECRET_KEY="!@#_)&^%$$epic8^%%$#@#%^&*(&^&"
-export TESTING_DATABASE_URI="postgresql://postgres@localhost/travis_tests"
+export test_db=dbname='travis_tests' user='postgres' password='' host='localhost'
